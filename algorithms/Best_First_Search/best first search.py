ad5aecdfd3e7180eea7294d6fcb5222e977daa5c
MoatazMoa <moatazmohmed98@gmail.com>
2025-12-11 22:14:59 +0200

    Create best first search.py

---
+++ algorithms/Best_First_Search/best first search.py
@@ -0,0 +1,41 @@
+import heapq
+
+class SolutionBestFS:
+    def heuristic(self, queens):
+        return -len(queens)
+
+    def solveNQueens(self, n: int):
+        res = []
+        pq = []
+        
+        start_state = (0, 0, [], set(), set(), set())
+        heapq.heappush(pq, start_state)
+        
+        while pq:
+            priority, row, queens, cols, posDiag, negDiag = heapq.heappop(pq)
+            
+            if row == n:
+                board = []
+                for c in queens:
+                    row_str = '.' * c + 'Q' + '.' * (n - c - 1)
+                    board.append(row_str)
+                res.append(board)
+                continue
+            
+            for c in range(n):
+                if c in cols or (row + c) in posDiag or (row - c) in negDiag:
+                    continue
+                
+                new_queens = queens + [c]
+                h = self.heuristic(new_queens)
+                
+                heapq.heappush(pq, (
+                    h,
+                    row + 1,
+                    new_queens,
+                    cols | {c},
+                    posDiag | {row + c},
+                    negDiag | {row - c}
+                ))
+        
+        return res
